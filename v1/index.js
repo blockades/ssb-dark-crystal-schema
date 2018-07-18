3f3a07bcc115582c2a3f7ada2ab239daceb310f6
Kieran <kieran.a.gibb@gmail.com>
2018-07-18 17:00:45 +0100

    correct syntax error

--- v1/index.js
+++ v1/index.js
@@ -2,6 +2,6 @@ module.exports = {
   isRoot: require('./root/sync/isRoot'),
   isRitual: require('./ritual/sync/isRitual'),
   isShard: require('./shard/sync/isShard'),
-  errorParser: require('./lib/errorParser')
+  errorParser: require('./lib/errorParser'),
   SCHEMA_VERSION: require('./version')
 }
