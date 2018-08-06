6d53ae0b7b783784c6ca679e67589daba6de3378
mixmix <whimful@gmail.com>
2018-08-06 16:39:07 +1200

    make validators pure functions

--- test/ritual.test.js
+++ test/ritual.test.js
@@ -1,11 +1,7 @@
 const fs = require('fs')
 const { describe } = require('tape-plus')
-
-const validator = require('is-my-json-valid')
-const schema = require('../v1/ritual/schema/ritual')
-const validate = validator(schema, { verbose: true })
-
 const { isRitual } = require('../v1/')
+const errorParser = require('../v1/lib/errorParser')
 
 describe('dark-crystal/ritual schema', context => {
   let ritual
@@ -15,39 +11,31 @@ describe('dark-crystal/ritual schema', context => {
   })
 
   context('is valid', assert => {
-    assert.ok(validate(ritual))
     assert.ok(isRitual(ritual))
 
     ritual.recps.map(recp => { return { link: recp, name: 'Bobo the Clown' } })
-    assert.ok(validate(ritual))
     assert.ok(isRitual(ritual))
   })
 
   context('invalid type', assert => {
     ritual.type = 'dark-smchystal/ritual'
-    assert.notOk(validate(ritual))
     assert.notOk(isRitual(ritual))
 
-    var errors = ritual.errors.map(e => `${e.field}: ${e.message}`)
-    assert.deepEqual(errors, ['data.type: pattern mismatch'])
+    assert.deepEqual(errorParser(isRitual), ['data.type: pattern mismatch'])
   })
 
   context('invalid version', assert => {
     ritual.version = 1
-    assert.notOk(validate(ritual))
     assert.notOk(isRitual(ritual))
 
-    var errors = ritual.errors.map(e => `${e.field}: ${e.message}`)
-    assert.deepEqual(errors, ['data.version: is the wrong type'])
+    assert.deepEqual(errorParser(isRitual), ['data.version: is the wrong type'])
   })
 
   context('invalid quorum', assert => {
     ritual.quorum = "3"
-    assert.notOk(validate(ritual))
     assert.notOk(isRitual(ritual))
 
-    var errors = ritual.errors.map(e => `${e.field}: ${e.message}`)
-    assert.deepEqual(errors, ['data.quorum: is the wrong type'])
+    assert.deepEqual(errorParser(isRitual), ['data.quorum: is the wrong type'])
 
     ritual.quorum = 3.2
     assert.notOk(isRitual(ritual))
@@ -58,11 +46,9 @@ describe('dark-crystal/ritual schema', context => {
 
   context('invalid shards', assert => {
     ritual.shards = "3"
-    assert.notOk(validate(ritual))
     assert.notOk(isRitual(ritual))
 
-    var errors = ritual.errors.map(e => `${e.field}: ${e.message}`)
-    assert.deepEqual(errors, ['data.shards: is the wrong type'])
+    assert.deepEqual(errorParser(isRitual), ['data.shards: is the wrong type'])
 
     ritual.shards = 1
     assert.notOk(isRitual(ritual))
@@ -70,19 +56,15 @@ describe('dark-crystal/ritual schema', context => {
 
   context('invalid tool', assert => {
     ritual.tool = { library: 'secrets.js' }
-    assert.notOk(validate(ritual))
     assert.notOk(isRitual(ritual))
 
-    var errors = ritual.errors.map(e => `${e.field}: ${e.message}`)
-    assert.deepEqual(errors, ['data.tool: is the wrong type'])
+    assert.deepEqual(errorParser(isRitual), ['data.tool: is the wrong type'])
   })
 
   context('invalid recps', assert => {
     ritual.recps = ['thisisnotafeedId']
-    assert.notOk(validate(ritual))
     assert.notOk(isRitual(ritual))
 
-    var errors = ritual.errors.map(e => `${e.field}: ${e.message}`)
-    assert.deepEqual(errors, ['data.recps: referenced schema does not match'])
+    assert.deepEqual(errorParser(isRitual), ['data.recps: referenced schema does not match'])
   })
 })
--- test/root.test.js
+++ test/root.test.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 const { describe } = require('tape-plus')
-const validator = require('is-my-json-valid')
-const schema = require('../v1/root/schema/root')
-const validate = validator(schema, { verbose: true })
 const errorParser = require('../v1/lib/errorParser')
-
 const { isRoot } = require('../v1/')
 
 describe('dark-crystal/root schema', context => {
@@ -15,44 +11,37 @@ describe('dark-crystal/root schema', context => {
   })
 
   context('root is valid', assert => {
-    assert.ok(validate(root))
     assert.ok(isRoot(root))
 
     root.recps.map(recp => { return { link: recp, name: 'Bobo the Clown' } })
-    assert.ok(validate(root))
     assert.ok(isRoot(root))
   })
 
   context('invalid type', assert => {
     root.type = 'dark-smchystal/root'
-    assert.notOk(validate(root))
     assert.notOk(isRoot(root))
-    assert.deepEqual(['data.type: pattern mismatch'], errorParser(root))
+    assert.deepEqual(['data.type: pattern mismatch'], errorParser(isRoot))
   })
 
   context('invalid version', assert => {
     root.version = 1
-    assert.notOk(validate(root))
     assert.notOk(isRoot(root))
-    assert.deepEqual(['data.version: is the wrong type'], errorParser(root))
+    assert.deepEqual(['data.version: is the wrong type'], errorParser(isRoot))
   })
 
   context('invalid name', assert => {
     root.name = { name: 'this is my name' }
-    assert.notOk(validate(root))
     assert.notOk(isRoot(root))
-    assert.deepEqual(['data.name: is the wrong type'], errorParser(root))
+    assert.deepEqual(['data.name: is the wrong type'], errorParser(isRoot))
   })
 
   context('invalid recps', assert => {
     root.recps = [...root.recps, ...root.recps]
-    assert.notOk(validate(root))
     assert.notOk(isRoot(root))
-    assert.deepEqual(['data.recps: has more items than allowed'], errorParser(root))
+    assert.deepEqual(['data.recps: has more items than allowed'], errorParser(isRoot))
 
     root.recps = ['thisisnotafeedId']
-    assert.notOk(validate(root))
     assert.notOk(isRoot(root))
-    assert.deepEqual(['data.recps.0: no (or more than one) schemas match'], errorParser(root))
+    assert.deepEqual(['data.recps.0: no (or more than one) schemas match'], errorParser(isRoot))
   })
 })
