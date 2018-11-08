57f17d0fe48d894cd2cf638ac6de79cb79d727c2
Kieran <kieran@tenthousandthings.org.uk>
2018-11-08 18:30:43 +0000

    roll out same hack... refactor later

--- validators/isRoot.js
+++ validators/isRoot.js
@@ -1,3 +1,17 @@
 const schemaFinder = require('../lib/schemaFinder')
+const validator = require('../lib/validator')
+const getContent = require('ssb-msg-content')
 
-module.exports = (msg) => schemaFinder('root')(msg)
+module.exports = function isRoot (msg) {
+  const content = getContent(msg)
+  var schema = schemaFinder('root')(content)
+  if (!schema) {
+    isRoot.errors = [{ field: 'version.name', message: 'is not present' }]
+    return false
+  }
+
+  var validate = validator(schema)
+  var result = validate(msg)
+  isRoot.errors = validate.errors
+  return result
+}
--- validators/isShard.js
+++ validators/isShard.js
@@ -1,3 +1,17 @@
 const schemaFinder = require('../lib/schemaFinder')
+const validator = require('../lib/validator')
+const getContent = require('ssb-msg-content')
 
-module.exports = (msg) => schemaFinder('shard')(msg)
+module.exports = function isShard (msg) {
+  const content = getContent(msg)
+  var schema = schemaFinder('shard')(content)
+  if (!schema) {
+    isShard.errors = [{ field: 'version.name', message: 'is not present' }]
+    return false
+  }
+
+  var validate = validator(schema)
+  var result = validate(msg)
+  isShard.errors = validate.errors
+  return result
+}
