7d1ddb826ea90ef2768174179eccc49ddad67c14
Nikolai B <nikolai-b@users.noreply.github.com>
2018-11-08 21:04:59 +0000

    Return is and version

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-
 const getContent = require('ssb-msg-content')
+const v1 = require('./v1')
+const v2 = require('./v2')
+
+const versionMap = {
+  [v1.SCHEMA_VERSION]: v1,
+  [v2.SCHEMA_VERSION]: v2,
+}
 
 module.exports = {
-  v1: require('./v1'),
-  v2: require('./v2'),
-  isRoot: function (root) {
-    if (getContent(root).version === '1.0.0') {
-      return require('./v1/root/sync/isRoot')(root)
-    } else return require('./v2/root/sync/isRoot')(root)
+  v1,
+  v2,
+  isRoot: function isRoot(root) {
+    const versionNumber = getContent(root).version
+    const vx = versionMap[versionNumber]
+    if (!!vx) {
+      const ret = vx.isRoot(root)
+      isRoot.errors = vx.isRoot.errors
+      return [ret, versionNumber]
+    } else {
+      isRoot.errors = [{ field: 'data.version', message: 'not recognised' }]
+      return [false, versionNumber]
+    }
   }
 }
--- test/isRoot.test.js
+++ test/isRoot.test.js
@@ -12,37 +12,37 @@ describe('dark-crystal/root schema', context => {
   })
 
   context('root is valid', assert => {
-    assert.ok(isRoot(root))
+    assert.deepEqual(isRoot(root), [true, '1.0.0'])
 
     root.recps.map(recp => { return { link: recp, name: 'Bobo the Clown' } })
-    assert.ok(isRoot(root))
+    assert.deepEqual(isRoot(root), [true, '1.0.0'])
   })
 
   context('invalid type', assert => {
     root.type = 'dark-smchystal/root'
-    assert.notOk(isRoot(root))
+    assert.deepEqual(isRoot(root), [false, '1.0.0'])
     assert.deepEqual(['data.type: pattern mismatch'], errorParser(isRoot))
   })
 
   context('invalid version', assert => {
-    root.version = 1
-    assert.notOk(isRoot(root))
-    assert.deepEqual(['data.version: is the wrong type'], errorParser(isRoot))
+    root.version = '3'
+    assert.deepEqual(isRoot(root), [false, '3'])
+    assert.deepEqual(['data.version: not recognised'], errorParser(isRoot))
   })
 
   context('invalid name', assert => {
     root.name = { name: 'this is my name' }
-    assert.notOk(isRoot(root))
+    assert.deepEqual(isRoot(root), [false, '1.0.0'])
     assert.deepEqual(['data.name: is the wrong type'], errorParser(isRoot))
   })
 
   context('invalid recps', assert => {
     root.recps = [...root.recps, ...root.recps]
-    assert.notOk(isRoot(root))
+    assert.deepEqual(isRoot(root), [false, '1.0.0'])
     assert.deepEqual(['data.recps: has more items than allowed'], errorParser(isRoot))
 
     root.recps = ['thisisnotafeedId']
-    assert.notOk(isRoot(root))
+    assert.deepEqual(isRoot(root), [false, '1.0.0'])
     assert.deepEqual(['data.recps.0: no (or more than one) schemas match'], errorParser(isRoot))
   })
 })
