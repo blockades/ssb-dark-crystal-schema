5a9902c6953a35b0255e7939c531266172e8e8d4
Kieran <kieran@tenthousandthings.org.uk>
2018-11-08 13:19:11 +0000

    copy over v1 to v2

---
+++ v2/shard/schema/shard.js
@@ -0,0 +1,32 @@
+const definitions = require('ssb-schema-definitions')
+const SCHEMA_VERSION = require('../../version')
+
+module.exports = {
+  $schema: 'http://json-schema.org/schema#',
+  type: 'object',
+  required: ['type', 'version', 'root', 'shard', 'recps'],
+  properties: {
+    type: {
+      type: 'string',
+      pattern: '^dark-crystal/shard$'
+    },
+    version: {
+      type: 'string',
+      pattern: `^${SCHEMA_VERSION}$`
+    },
+    root: { $ref: '#/definitions/messageId' },
+    shard: { $ref: '#/definitions/encrypt/box' },
+    recps: {
+      type: 'array',
+      maxItems: 2,
+      minItems: 2,
+      items: {
+        oneOf: [
+          { $ref: '#/definitions/feedId' },
+          { $ref: '#/definitions/mentions/feed' }
+        ]
+      }
+    }
+  },
+  definitions: definitions
+}
