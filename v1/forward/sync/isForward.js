1b4e8623cc826ac3e19db6af771478ed294eb768
Kieran <kieran@tenthousandthings.org.uk>
2018-11-08 12:44:13 +0000

    add schema definition for isForward

---
+++ v1/forward/sync/isForward.js
@@ -0,0 +1,4 @@
+const validator = require('../../lib/validator')
+const schema = require('../schema/forward')
+
+module.exports = validator(schema)
--- v1/index.js
+++ v1/index.js
@@ -2,6 +2,7 @@ module.exports = {
   isRoot: require('./root/sync/isRoot'),
   isRitual: require('./ritual/sync/isRitual'),
   isShard: require('./shard/sync/isShard'),
+  isForward: require('./forward/sync/isForward'),
   errorParser: require('./lib/errorParser'),
   SCHEMA_VERSION: require('./version')
 }
