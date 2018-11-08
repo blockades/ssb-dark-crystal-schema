0e0e0cfc33ee7414d2a58f17eae7626e012b9f58
Kieran <kieran@tenthousandthings.org.uk>
2018-11-08 18:19:35 +0000

    horrible hack but is passes the errors down the chain

--- validators/isRitual.js
+++ validators/isRitual.js
@@ -1,6 +1,17 @@
 const schemaFinder = require('../lib/schemaFinder')
+const validator = require('../lib/validator')
+const getContent = require('ssb-msg-content')
 
-module.exports = (msg) => {
-  var schema = schemaFinder('ritual')
-  return schema(msg)
+module.exports = function isRitual (msg) {
+  const content = getContent(msg)
+  var schema = schemaFinder('ritual')(content)
+  if (!schema) {
+    isRitual.errors = [{ field: 'version.name', message: 'is not present' }]
+    return false
+  }
+
+  var validate = validator(schema)
+  var result = validate(msg)
+  isRitual.errors = validate.errors
+  return result
 }
