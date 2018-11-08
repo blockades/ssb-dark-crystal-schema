a7bc8b4806d53c2f08b72968ef37d8613f6f5dd4
Kieran <kieran@tenthousandthings.org.uk>
2018-11-08 13:19:52 +0000

    export high-level fns that check version before calling lower fn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = require('./v1')
+const versions = require('./versions')
+const getContent = require('ssb-msg-content')
+
+const isRoot = (msg) => {
+  const content = getContent(msg)
+  return version !== undefined && version.isRoot(msg)
+}
+
+const isRitual = (msg) => {
+  const content = getContent(msg)
+  const version = versions[content.message]
+  return version !== undefined && version.isRitual(msg)
+}
+
+const isShard = (msg) => {
+  const content = getContent(msg)
+  const version = versions[content.message]
+  return version !== undefined && version.isShard(msg)
+}
+
+// %%TODO%% Refactor into a fn that returns the correct fn
+
+module.exports = {
+  isShard,
+  isRoot,
+  isRitual
+}
