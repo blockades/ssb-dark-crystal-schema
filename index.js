6a282ebf0e8d658aa07c0dd06f3d78a138df0a39
ameba23 <ameba23@systemli.org>
2018-11-08 12:16:52 +0100

    attempt as creating a generic isRoot

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+
+const getContent = require('ssb-msg-content')
+
 module.exports = {
   v1: require('./v1'),
-  v2: require('./v2')
+  v2: require('./v2'),
+  isRoot: function (root) {
+    if (getContent(root).version === '1.0.0') {
+      return require('./v1/root/sync/isRoot')(root)
+    } else return require('./v2/root/sync/isRoot')(root)
+  }
 }
