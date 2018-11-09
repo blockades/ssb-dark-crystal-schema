e33effc5d8caee9cfbf4e0ac06ca57d9185dea9d
Kieran <kieran@tenthousandthings.org.uk>
2018-11-09 12:31:25 +0000

    integrate forward schema and validator

--- schemas/v1/index.js
+++ schemas/v1/index.js
@@ -1,11 +1,12 @@
 const ritual = require('./ritual')
 const root = require('./root')
 const shard = require('./shard')
+const forward = require('./forward')
 
 module.exports = {
   SCHEMA_VERSION: require('./version'),
   getSchema: (schemaName) => {
-    let schemas = { ritual, root, shard }
+    let schemas = { ritual, root, shard, forward }
     return schemas[schemaName]
   }
 }
