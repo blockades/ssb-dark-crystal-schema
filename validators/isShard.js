fac24cc49937fd98b6571401a5f5e44cd1bdd4c7
Kieran <kieran@tenthousandthings.org.uk>
2018-11-09 12:18:19 +0000

    pass down inherited errors to validator

--- validators/isShard.js
+++ validators/isShard.js
@@ -1,17 +1,20 @@
-const schemaFinder = require('../lib/schemaFinder')
+const findSchemaByName = require('../lib/schemaFinder')
 const validator = require('../lib/validator')
 const getContent = require('ssb-msg-content')
 
 module.exports = function isShard (msg, opts = {}) {
+  isShard.errors = []
   const content = getContent(msg)
-  var schema = schemaFinder('shard')(content)
-  if (!schema) {
-    isShard.errors = [{ field: 'data.version', message: 'is not present' }]
-    return false
-  }
+  var findSchemaByVersion = findSchemaByName('shard')
+  var schema = findSchemaByVersion(content)
 
-  var validate = validator(schema)
-  var result = validate(msg, opts = {})
-  isShard.errors = validate.errors
-  return result
+  if (!schema) {
+    isShard.errors = isShard.errors.concat(findSchemaByVersion.errors)
+    return false
+  } else {
+    var validate = validator(schema)
+    var result = validate(msg, opts)
+    isShard.errors = validate.errors
+    return result
+  }
 }
