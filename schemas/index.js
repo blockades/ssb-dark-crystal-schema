43eed26d2198db8b85dd5ff495a272eaeff76567
Kieran <kieran@tenthousandthings.org.uk>
2018-11-09 12:23:18 +0000

    add a currentVersion function determined by schemas/index.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,10 @@
+const V1_SCHEMA_VERSION = require('./v1/version')
+const V2_SCHEMA_VERSION = require('./v2/version')
+
 module.exports = {
-  "1.0.0": require('./v1'),
-  "2.0.0": require('./v2')
+  versions: {
+    "1.0.0": require('./v1'),
+    "2.0.0": require('./v2')
+  },
+  currentVersion: () => V1_SCHEMA_VERSION
 }
