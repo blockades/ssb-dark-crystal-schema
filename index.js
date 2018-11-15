868d4034e037c27407018db82d2fcdb0bcd8012f
Kieran <kieran@tenthousandthings.org.uk>
2018-11-15 13:37:22 +0000

    export errorParser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const validators = require('./validators')
+const errorParser = require('./lib/errorParser')
 
 module.exports = Object.assign(
-  { SCHEMA_VERSION: '1.0.0' }, // current schema version
-  validators
+  {
+    SCHEMA_VERSION: '1.0.0', // current schema version
+    errorParser
+  },
+  validators,
 )
