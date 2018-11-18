4b4ae2bc17eb49e729ce5e5ccd74321d1278b641
ameba23 <ameba23@systemli.org>
2018-11-18 18:48:40 +0100

    update tests for v2

---
+++ test/v2/forward.test.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const { join } = require('path')
+const { describe } = require('tape-plus')
+const errorParser = require('../../lib/errorParser')
+const { isForward } = require('../..')
+
+describe('dark-crystal/forward schema', context => {
+  let forward
+
+  context.beforeEach(c => {
+    forward = JSON.parse(fs.readFileSync(join(__dirname, 'fixtures/forward.json'), 'utf8'))
+  })
+
+  context('forward is valid', assert => {
+    assert.ok(isForward(forward))
+  })
+
+  context('invalid type', assert => {
+    forward.type = 'dark-smchystal/forward'
+    assert.notOk(isForward(forward))
+
+    assert.deepEqual(errorParser(isForward), ['data.type: pattern mismatch'])
+  })
+
+  context('invalid version', assert => {
+    forward.version = 1
+    assert.notOk(isForward(forward))
+
+    assert.deepEqual(errorParser(isForward), ['data.version: is not a valid version'])
+  })
+
+  context('invalid shard', assert => {
+    forward.shard = 2
+    assert.notOk(isForward(forward))
+
+    assert.deepEqual(errorParser(isForward), ['data.shard: is the wrong type'])
+  })
+
+  context('invalid recps', assert => {
+    forward.recps = ['thisisnotafeedId', 'nor is this']
+    assert.notOk(isForward(forward))
+
+    assert.deepEqual(errorParser(isForward), ['data.recps.0: no (or more than one) schemas match', 'data.recps.1: no (or more than one) schemas match'])
+  })
+})
---
+++ test/v2/ritual.test.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const { join } = require('path')
+const { describe } = require('tape-plus')
+const errorParser = require('../../lib/errorParser')
+const { isRitual } = require('../..')
+
+describe('dark-crystal/ritual schema', context => {
+  let ritual
+
+  context.beforeEach(c => {
+    ritual = JSON.parse(fs.readFileSync(join(__dirname, 'fixtures/ritual.json'), 'utf8'))
+  })
+
+  context('is valid', assert => {
+    assert.ok(isRitual(ritual))
+  })
+
+  context('invalid type', assert => {
+    ritual.type = 'dark-smchystal/ritual'
+    assert.notOk(isRitual(ritual))
+    assert.deepEqual(errorParser(isRitual), ['data.type: pattern mismatch'])
+  })
+
+  context('can attach errors to tested object', assert => {
+    ritual.type = 'dark-smchystal/ritual'
+    assert.notOk(isRitual(ritual, { attachErrors: true }))
+    assert.deepEqual(errorParser(ritual), ['data.type: pattern mismatch'])
+  })
+
+  context('invalid version', assert => {
+    ritual.version = 1
+    assert.notOk(isRitual(ritual))
+
+    assert.deepEqual(errorParser(isRitual), ['data.version: is not a valid version'])
+  })
+
+  context('invalid quorum', assert => {
+    ritual.quorum = '3'
+    assert.notOk(isRitual(ritual))
+
+    assert.deepEqual(errorParser(isRitual), ['data.quorum: is the wrong type'])
+
+    ritual.quorum = 3.2
+    assert.notOk(isRitual(ritual))
+
+    ritual.quorum = 1
+    assert.notOk(isRitual(ritual))
+  })
+
+  context('invalid shards', assert => {
+    ritual.shards = '3'
+    assert.notOk(isRitual(ritual))
+
+    assert.deepEqual(errorParser(isRitual), ['data.shards: is the wrong type'])
+
+    ritual.shards = 1
+    assert.notOk(isRitual(ritual))
+  })
+
+  context('invalid tool', assert => {
+    ritual.tool = { library: 'secrets.js' }
+    assert.notOk(isRitual(ritual))
+
+    assert.deepEqual(errorParser(isRitual), ['data.tool: is the wrong type'])
+  })
+
+  context('invalid recps', assert => {
+    ritual.recps = ['thisisnotafeedId']
+    assert.notOk(isRitual(ritual))
+
+    assert.deepEqual(errorParser(isRitual), ['data.recps: referenced schema does not match'])
+  })
+})
