8212f0c152f5817169727b68708bab43c7382880
ameba23 <ameba23@systemli.org>
2018-11-18 18:34:54 +0100

    bump current version in index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const errorParser = require('./lib/errorParser')
 
 module.exports = Object.assign(
   {
-    SCHEMA_VERSION: '1.0.0', // current schema version
+    SCHEMA_VERSION: '2.0.0', // current schema version
     errorParser
   },
-  validators,
+  validators
 )
