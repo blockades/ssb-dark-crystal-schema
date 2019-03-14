c76d55141324b6dc121d79168a220f362355ece2
ameba23 <ameba23@systemli.org>
2019-03-14 20:06:16 +0100

    comment

--- schemas/request/v1.js
+++ schemas/request/v1.js
@@ -2,6 +2,7 @@ const merge = require('lodash.merge')
 const inviteSchema = require('ssb-invite-schema/src/v1/invite/schema/invite.js')
 const isCanonicalBase64 = require('is-canonical-base64')
 
+// TODO: maybe we should add a regex for curve25519 keys to ssb-ref
 const requestSchema = merge({}, inviteSchema)
 inviteSchema.properties.ephPublicKey = {
   type: 'string',
