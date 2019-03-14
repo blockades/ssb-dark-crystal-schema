a7343ca7b46174322888f30892cb9b45e30ebcfb
ameba23 <ameba23@systemli.org>
2019-03-14 19:57:22 +0100

    add ephPublicKey property to request schema

--- schemas/request/v1.js
+++ schemas/request/v1.js
@@ -1,3 +1,11 @@
-const inviteSchema = require('ssb-invite-schema/src/v1/invite/schema/invite.js')
+const merge = require('lodash.merge')
+const _inviteSchema = require('ssb-invite-schema/src/v1/invite/schema/invite.js')
+const isCanonicalBase64 = require('is-canonical-base64')
+
+const inviteSchema = merge({}, _inviteSchema)
+inviteSchema.properties.ephPublicKey = {
+  type: 'string',
+  pattern: isCanonicalBase64(null, '\.curve25519', 32)
+}
 
 module.exports = inviteSchema
